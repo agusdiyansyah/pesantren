cada21441227fc966065bcb2b997a4205a655857
agusdiyansyah <kreasiin@gmail.com>
2015-09-24 14:30:40 +0700

    + off button for log out

--- module/engine/home/global.js
+++ module/engine/home/global.js
@@ -1,7 +1,25 @@
+$(document).ready(function() {
+	$('.off').click(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: module+'user/ajax.php',
+            type: 'post',
+            dataType: 'json',
+            data: {ac: 'off'},
+            success: function (json) {
+                if (json.stat) {
+                    window.location.href = 'login';
+                };
+            }
+        });
+        
+    });
+});
+
 function total (tag, ajax) {
 	$.getJSON(ajax, {ac: 'total'}, function(json) {
 		if (json.stat) {
-			$(tag).html(json.total);
+			$(tag).html('Rp.'+json.total);
 		} else{
 			$(tag).html(0);
 		};
