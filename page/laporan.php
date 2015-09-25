413281a59854d5e4d1ddeaaf1877e1b1eb2b3463
agusdiyansyah <kreasiin@gmail.com>
2015-09-26 00:43:10 +0700

    + update laporan view

--- page/laporan.php
+++ page/laporan.php
@@ -1,9 +1,55 @@
-<div class="box box-solid">
-    <div class="box-header with-border">
-        <i class="ion ion-ios-list-outline"></i>
-        <h3 class="box-title">Laporan</h3>
-    </div><!-- /.box-header -->
+<div class="nav-tabs-custom">
+    <ul class="nav nav-tabs pull-right">
+    	<li>
+	        <div class="form-group" style="display:inline-block; width:300px;margin-bottom: 0px;margin-right: 5px;">
+                <form class='form-date'>
+                    <div class='input-group'>
+                        <span class="input-group-btn">
+                            <button type="button" class="btn btn-default btn-print"><i class="fa fa-print"></i></button>
+                        </span>
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-calendar">
+                            </span>
+                        </span>
+                        <input type='text' class="form-control date" name="src"/>
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+            
+    	</li>
+      	<li class="pull-left">
+            <div class="form-group" style="display:inline-block; width:300px;margin-bottom: 0px;margin-left: 5px;">
+                <div class='input-group'>
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-default load-list"><i class="ion ion-ios-list-outline"></i></button>
+                        <button type="submit" class="btn btn-default load-img"><i class="ion ion-image"></i></button>
+                    </span>
+                </div>
+            </div>
+        </li>
+    </ul>
+
+    <input type="hidden" name="show" value="list" class='tampil'>
+
     <div class="box-body">
-        
-    </div><!-- /.box-body -->
+    	
+    </div>
 </div>
+<?php  
+    $now = date('Y').'-'.date('m');
+	$js = "
+	<script>
+		// $('body').removeClass('sidebar-collapse');
+        var engine   = '".module."laporan/ajax.php';
+        var module  = '".module."';
+        var vendor  = '".vendor."';
+        var date    = '".$now."';
+	</script>
+    <script src='".vendor."plugins/jquery-validation/jquery.validate.min.js'></script>
+    <script src='".module."laporan/render.js'></script>
+    <script src='".module."laporan/index.js'></script>
+	";
+?>
