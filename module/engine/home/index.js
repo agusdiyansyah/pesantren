5275ea65a8240659e580fc91ddd61c71e60be51a
agusdiyansyah <kreasiin@gmail.com>
2015-09-26 12:35:05 +0700

    + home: update render in js function

--- module/engine/home/index.js
+++ module/engine/home/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	render(date);
+	render(date, '.box-body');
 	total('.info-debit .inner h3', debit);
 	total('.info-kredit .inner h3', kredit);
 	total('.info-saldo .inner h3', saldo);
