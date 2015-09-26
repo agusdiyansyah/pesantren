020f8c491e8b34c3cb6cd56b6aaed6c618c3d46c
agusdiyansyah <kreasiin@gmail.com>
2015-09-26 12:39:41 +0700

    + laporan page: add some button to navigate laporan

--- page/laporan.php
+++ page/laporan.php
@@ -1,25 +1,50 @@
 <div class="nav-tabs-custom">
     <ul class="nav nav-tabs pull-right">
     	<li>
-	        <div class="form-group" style="display:inline-block; width:300px;margin-bottom: 0px;margin-right: 5px;">
+            <div class="form-group" style="display:inline-block; width:300px;margin-bottom: 0px;margin-right: 5px;">
                 <form class='form-date'>
                     <div class='input-group'>
                         <span class="input-group-btn">
-                            <button type="button" class="btn btn-default btn-print"><i class="fa fa-print"></i></button>
+                            <button type="button" class="btn btn-default btn-print">
+                                <i class="ion ion-ios-printer-outline"></i>
+                            </button>
                         </span>
+
                         <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-calendar">
+                            <i class="ion ion-ios-calendar-outline"></i>
                             </span>
                         </span>
+
                         <input type='text' class="form-control date" name="src"/>
+
                         <span class="input-group-btn">
-                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                            <button type="submit" class="btn btn-default">
+                                <i class="fa fa-search"></i>
+                            </button>
                         </span>
                     </div>
                 </form>
             </div>
             
-    	</li>
+        </li>
+        <li>
+            <div class="form-group" style="display:inline-block; width:280px;margin-bottom: 0px;margin-right: 5px;">
+                <div class='input-group'>
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-default btn-all">
+                            <i class="ion ion-social-usd-outline"></i> &nbspSemua transaksi
+                        </button>
+                        <button type="submit" class="btn btn-default btn-debit">
+                            <i class="ion ion-ios-download-outline"></i> &nbspDebit
+                        </button>
+                        <button type="submit" class="btn btn-default btn-kredit">
+                            <i class="ion ion-ios-upload-outline"></i> &nbspKredit
+                        </button>
+                    </span>
+                </div>
+            </div>
+            
+        </li>
       	<li class="pull-left">
             <div class="form-group" style="display:inline-block; width:300px;margin-bottom: 0px;margin-left: 5px;">
                 <div class='input-group'>
@@ -32,7 +57,7 @@
         </li>
     </ul>
 
-    <input type="hidden" name="show" value="list" class='tampil'>
+    <input type="hidden" name="type" value="" class='type'>
 
     <div class="box-body">
     	
