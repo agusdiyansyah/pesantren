2b75cca5ec8f8df77cb59bb35514ed33836f68a0
agusdiyansyah <kreasiin@gmail.com>
2015-09-26 12:37:07 +0700

    + laporan: optimizing js methode

--- module/engine/laporan/index.js
+++ module/engine/laporan/index.js
@@ -1,44 +1,26 @@
 $(document).ready(function() {
-	var tampil = $('.tampil').val();
-	buka(tampil);
-	render(date);
+	var type 	= $('.type').val();
+	var lap    	= render(date, '.box-body', type);
 
 	$('.load-list').click(function(event) {
 		event.preventDefault();
-		$('.tampil').val('list');
-		buka('list');
-		render(date);
+		var type 	= $('.type').val();
+		render(date, '.box-body', type);
 	});
 
 	$('.load-img').click(function(event) {
 		event.preventDefault();
 		$('.tampil').val('img');
-		$.getJSON(engine, {ac: 'img', src: date}, function(json) {
-			var tag = '';
-			$.each(json, function(index, data) {
-				tag += ''+
-						'<img src="'+vendor+'dist/file/'+data+'" >'+
-				'';
-			});
-			$('.box-body').html(tag);
-		});
+		render_img_all(date);
 	});
 
 	$('.form-date').submit(function(event) {
 		event.preventDefault();
 		var tampil = $('.tampil').val();
 		if (tampil == 'list') {
-			render($('.date').val());			
+			render($('.date').val(), '.box-body');			
 		} else{
-			$.getJSON(engine, {ac: 'img', src: $('.date').val()}, function(json) {
-				var tag = '';
-				$.each(json, function(index, data) {
-					tag += ''+
-							'<img src="'+vendor+'dist/file/'+data+'" >'+
-					'';
-				});
-				$('.box-body').html(tag);
-			});
+			render_img_all($('.date').val());
 		};
 	});
 
@@ -50,10 +32,41 @@ $(document).ready(function() {
     $('.btn-print').click(function(event) {
     	event.preventDefault();
     	var data = $('.box-body').html();
-    	window.location.href = 'http://localhost/pesantren/module/engine/laporan/ajax.php?ac=cetak&vendor='+vendor+'&data='+data;
+    	var url  = 'http://localhost/pesantren/module/engine/laporan/ajax.php?ac=cetak&vendor='+vendor+'&data='+data;
+    	var win = window.open(url, '_blank');
+		win.focus();
+    });
+
+    $('.btn-all').click(function(event) {
+    	event.preventDefault();
+    	$('.type').val('');
+    	$('.load-img').css('display', 'inline-block');
+    	render(date, '.box-body');
+    });
+
+    $('.btn-debit').click(function(event) {
+    	event.preventDefault();
+    	$('.type').val(1);
+    	$('.load-img').css('display', 'none');
+    	render(date, '.box-body', 1);
+    });
+
+    $('.btn-kredit').click(function(event) {
+    	event.preventDefault();
+    	$('.type').val(2);
+    	$('.load-img').css('display', 'inline-block');
+    	render(date, '.box-body', 2);
     });
 });
 
-function buka (page) {
-	$('.box-body').load(module+'laporan/'+page+'.php');
+function render_img_all (tgl, type) {
+	$.getJSON(engine, {ac: 'img', src: tgl, type: type}, function(json) {
+		var tag = '';
+		$.each(json, function(index, data) {
+			tag += ''+
+					'<img src="'+vendor+'dist/file/'+data+'" >'+
+			'';
+		});
+		$('.box-body').html(tag);
+	});
 }
